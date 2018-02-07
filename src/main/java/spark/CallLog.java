7fcb071fd5c477c9f6c1ceebe8a46f0de4a8d10a
dongkelun <1412359494@qq.com>
2018-02-07 11:43:14 +0800

    test

--- src/main/java/spark/CallLog.java
+++ src/main/java/spark/CallLog.java
@@ -3,9 +3,10 @@ package spark;
 import java.io.Serializable;
 
 public class CallLog implements Serializable {
-  public String callsign;
-  public Double contactlat;
-  public Double contactlong;
-  public Double mylat;
-  public Double mylong;
+	private static final long serialVersionUID = 2597105495282743206L;
+	public String callsign;
+	public Double contactlat;
+	public Double contactlong;
+	public Double mylat;
+	public Double mylong;
 }
